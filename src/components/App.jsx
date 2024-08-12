9e4ac546ef8c7008bfd0119f707667e9073060b3
Vladyslav Venher <vladyslavvenher@gmail.com>
2024-08-12 16:04:08 +0300

    do h/w

---
+++ src/components/App.jsx
@@ -0,0 +1,118 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+// function Product() {
+//   return (
+//     <div>
+//       <h2>Cookies</h2>
+//       <p>Price: 999 credits</p>
+//     </div>
+//   );
+// }
+
+// export default function App() {
+//   return (
+//     <div>
+//       <h1>Products</h1>
+
+//       <Product />
+//       <Product />
+//       <Product />
+//     </div>
+//   );
+// }
+
+// import Product from './Product';
+
+// export default function App() {
+//   return (
+//     <div>
+//       <h1>Best selling</h1>
+
+//       <Product />
+//       <Product />
+//       <Product />
+//     </div>
+//   );
+// }
+
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <div>
+//         <a href="https://vitejs.dev" target="_blank">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://react.dev" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Vite + React</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.jsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//     </>
+//   )
+// }
+
+// export default App
+
+
+// import Product from "./Product";
+
+// export default function App() {
+//   return (
+//     <div>
+//       <h1>Best selling</h1>
+
+//       <Product
+//         name="Tacos With Lime"
+//         imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
+//         price={10.99}
+//       />
+//       <Product
+//         name="Fries and Burger"
+//         imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
+//         price={14.29}
+//       />
+//     </div>
+//   );
+// }
+
+// const favouriteBooks = [
+//   { id: "id-1", name: "JS for beginners" },
+//   { id: "id-2", name: "React basics" },
+//   { id: "id-3", name: "React Router overview" }
+// ];
+
+// const BookList = ({ books }) => {
+//   return (
+// 	<ul>
+//     {books.map((book) => {
+// 		return <li key={book.id}>{book.name}</li>;
+//     })}
+// 	</ul>
+//   );
+// };
+
+// const App = () => {
+//   return (
+//     <>
+//     <h1>Books of the week</h1>
+//       <BookList books={favouriteBooks} />
+//     </>
+//   );
+// };
---
+++ src/components/Product.jsx
@@ -0,0 +1,59 @@
+// export default function Product() {
+//   return (
+//     <div>
+//       <h2>Tacos</h2>
+//         <p>Price: 999 credits</p>
+//     </div>
+//   );
+// }
+
+// export default function Product() {
+//   return (
+//     <div>
+//       <h2>Tacos</h2>
+//       <img src="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640" alt="Tacos With Lime" width="640" />
+//       <p>Price: 999 credits</p>
+//     </div>
+//   );
+// }
+
+// export default function Product() {
+//   return (
+//       <h2>Tacos</h2>
+//       <img src="<https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640>" alt="Tacos With Lime" width="640" />
+//       <p>Price: 999 credits</p>
+//   );
+// };
+
+// export default function Product() {
+//   return (
+//     <div>
+//       <h2>Tacos</h2>
+//       <img src="<https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640>" alt="Tacos With Lime" width="640" />
+//       <p>Price: 999 credits</p>
+//     </div>
+//   );
+// }
+
+// export default function Product(props) {
+//   return (
+//     <div>
+//       <h2>{props.name}</h2>
+//       <img src={props.imgUrl} alt={props.name} width="480" />
+//       <p>Price: {props.price} credits</p>
+//     </div>
+//   );
+// }
+
+// export default function Product({ name, imgUrl, price }) {
+//   return (
+//     <div>
+//       <h2>{name}</h2>
+//       <img src={imgUrl} alt={name} width="480" />
+//       <p>Price: {price} credits</p>
+//     </div>
+//   );
+// }
+
+const popa = "123"
+const dupa = '12345'
